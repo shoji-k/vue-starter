dc982abd5cd829cf378f1cffd1264308b5fae788
shoji-k <shoji.kobayashi.0125@gmail.com>
2019-08-12 20:13:03 +0900

    can add and delete todo

--- src/components/pages/TodoList.vue
+++ src/components/pages/TodoList.vue
@@ -3,13 +3,23 @@
     <h2>Todo List</h2>
     <div class="working-list">
       <ul>
-        <li v-for="todo in working" :key="todo.id">{{ todo.name }}</li>
+        <li v-for="todo in working" :key="todo.id">
+          <input type="checkbox" value="todo.id" @click="toggleDone(todo)" />
+          {{ todo.name }}
+          <a href="#" @click="deleteTask(todo)">Delete</a>
+        </li>
       </ul>
+      <input type="text" v-model="newTaskName" />
+      <button @click="addTask">Add</button>
     </div>
     <h2>Done</h2>
     <div class="done-list">
       <ul>
-        <li v-for="todo in done" :key="todo.id">{{ todo.name }}</li>
+        <li v-for="todo in done" :key="todo.id">
+          <input type="checkbox" value="todo.id" @click="toggleDone(todo)" />
+          {{ todo.name }}
+          <a href="#" @click="deleteTask(todo)">Delete</a>
+        </li>
       </ul>
     </div>
   </div>
@@ -20,6 +30,8 @@ export default {
   name: 'TodoList',
   data: function () {
     return {
+      newTaskName: '',
+      nextTodoId: 4,
       todos: [
         {
           id: 1,
@@ -47,7 +59,29 @@ export default {
       return this.todos.filter(todo => todo.done)
     }
   },
-  methods: {}
+  methods: {
+    toggleDone: function (todo) {
+      const id = todo.id
+      const found = this.todos.find(todo => todo.id === id)
+      if (!found) {
+        return false
+      }
+      found.done = !found.done
+    },
+    addTask: function () {
+      this.todos.push({
+        id: this.nextTodoId,
+        name: this.newTaskName,
+        done: false
+      })
+      this.newTaskName = ''
+      this.nextTodoId++
+    },
+    deleteTask: function (todo) {
+      const id = todo.id
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
+  }
 }
 </script>
 
